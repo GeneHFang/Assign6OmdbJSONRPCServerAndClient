56f32c266ce429995bab8b1f32df9c8dc0010dca
GeneHFang <gene.li@nyu.edu>
2020-05-01 20:20:40 -0400

    fixed getLibrary

--- Assign6/src/java/client/SeriesSeasonTcpProxy.java
+++ Assign6/src/java/client/SeriesSeasonTcpProxy.java
@@ -82,7 +82,7 @@ public class SeriesSeasonTcpProxy extends Object implements SeriesLibrary {
         int i = 0;
         String request = "";
            while (true){
-            numBytesReceived = is.read(bytesReceived, i, bufLen);
+            numBytesReceived = is.read(bytesReceived, i, bufLen-i);
             if (numBytesReceived == -1) {break;}
             request = request + new String(bytesReceived,0,numBytesReceived);
             i = i + numBytesReceived;
